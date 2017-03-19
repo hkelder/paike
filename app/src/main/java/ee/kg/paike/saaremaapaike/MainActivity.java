276829e317451a7c55bea89afcecd4cd579c3381
RiivoXD\ProoviAinult <riivotammoja@gmail.com>
2017-03-19 14:16:32 +0200

    Fixitud

--- app/src/main/java/ee/kg/paike/saaremaapaike/MainActivity.java
+++ app/src/main/java/ee/kg/paike/saaremaapaike/MainActivity.java
@@ -4,9 +4,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.FrameLayout;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
