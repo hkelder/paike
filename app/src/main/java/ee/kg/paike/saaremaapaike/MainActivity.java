7499a3abdccd376c00d34f95abbc4cb9aab7a8af
Mikk Kärner <mikk.karner@proekspert.ee>
2017-03-19 13:35:15 +0200

    Tühi events fragment

--- app/src/main/java/ee/kg/paike/saaremaapaike/MainActivity.java
+++ app/src/main/java/ee/kg/paike/saaremaapaike/MainActivity.java
@@ -1,5 +1,7 @@
 package ee.kg.paike.saaremaapaike;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        EventsFragment fragment = new EventsFragment();
+        fragmentTransaction.replace(R.id.main_activity_holder, fragment).commit();
+       
     }
 }
