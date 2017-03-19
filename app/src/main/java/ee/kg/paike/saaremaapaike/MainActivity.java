5679f354585d06cd102065551933935b47351cde
Mikk Kärner <mikk.karner@proekspert.ee>
2017-03-19 15:14:58 +0200

    Fragment lõpetamine

--- app/src/main/java/ee/kg/paike/saaremaapaike/MainActivity.java
+++ app/src/main/java/ee/kg/paike/saaremaapaike/MainActivity.java
@@ -1,24 +1,55 @@
 package ee.kg.paike.saaremaapaike;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.navbottom)
+    BottomNavigationView bottomNavigationView;
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        EventsFragment fragment = new EventsFragment();
+        openFragment(new EventsFragment());
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_events:
+                        openFragment(new EventsFragment());
+                        break;
+                    case R.id.menu_food:
+                        openFragment(new FoodFragment());
+                        break;
+                    case R.id.menu_accommodation:
+                        openFragment(new AccommodationFragment());
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+    }
+
+    public void openFragment(Fragment fragment) {
+        if (fragmentManager == null) fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.main_activity_holder, fragment).commit();
-       
     }
 }
