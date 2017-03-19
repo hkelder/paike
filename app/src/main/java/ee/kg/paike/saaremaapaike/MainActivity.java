799534c684c4627ce2de10bec8a11444e6e10d7c
RiivoXD\ProoviAinult <riivotammoja@gmail.com>
2017-03-19 14:11:24 +0200

    ButterKnife implementation.

--- app/src/main/java/ee/kg/paike/saaremaapaike/MainActivity.java
+++ app/src/main/java/ee/kg/paike/saaremaapaike/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
