28ee5d5cf3178ad537ce17fa17d7f95ab31e8991
Henri Kelder <henrikelder@gmail.com>
2017-04-02 11:10:50 +0300

    Majutuse Java failid

---
+++ app/src/main/java/ee/kg/paike/saaremaapaike/Accommodation.java
@@ -0,0 +1,16 @@
+package ee.kg.paike.saaremaapaike;
+
+class Accommodation {
+
+    String title;
+    String subtitle;
+    String details;
+
+    Accommodation(String title, String subtitle, String details) {
+
+        this.title = title;
+        this.subtitle = subtitle;
+        this.details = details;
+
+    }
+}
